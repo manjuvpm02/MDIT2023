72a5ed51dcf389f3686383cb695613ed2bd8d000
Manju M <manjuvpm12@gmail.com>
2023-02-24 17:37:35 +0530

    Assignment 8

--- Week 1 - Assignment/src/Assignment8/Employee.java
+++ Week 1 - Assignment/src/Assignment8/Employee.java
@@ -1,5 +1,34 @@
 package Assignment8;
 
+
 public class Employee {
+	
+	private String name, hireDay;
+	private double salary;
+	
+	public Employee(String name, double salary, String hireDay)
+	{
+		
+		this.name=name;
+		this.salary=salary;
+		this.hireDay=hireDay;
+		
+	}
+	
+
+	public void raiseSalary()
+	{
+		
+		salary=salary+salary*5/100;		
+		
+	}
+	
+	public void EmployeeData()
+	{
+		System.out.println("name="+name+","+"salary="+salary+","+"hireDay="+hireDay);
+	}
+	
 
 }
+
+
---
+++ Week 1 - Assignment/src/Assignment8/EmployeeMain.java
@@ -0,0 +1,35 @@
+package Assignment8;
+
+public class EmployeeMain {
+
+	
+	public static void main(String[] args) 
+	{
+		
+		Employee[] empData = new Employee[3];
+		
+		empData[0]= new Employee("Carl Cracker",78750.0,"1987-12-15");
+		empData[1]= new Employee("Harry Hacker",52500.0,"1989-10-01");
+		empData[2]= new Employee("Tony Tester",42000.0,"1990-03-15");
+		
+		for(Employee employeeDetails: empData)
+		{
+			employeeDetails.EmployeeData();
+				
+		}
+		
+		System.out.println();
+		System.out.println("Salary after salary increment of 5%");
+		
+		
+		for(Employee employeeDetails:empData)
+		{
+			employeeDetails.raiseSalary();
+			
+			employeeDetails.EmployeeData();	
+		}
+		
+	}
+	
+	
+}
