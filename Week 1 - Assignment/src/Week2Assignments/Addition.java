e833be0cf030c8206b24739df4fb209704f25da0
Manju M <manjuvpm12@gmail.com>
2023-03-02 19:12:24 +0530

    Week 2 Assignments

---
+++ Week 1 - Assignment/src/Week2Assignments/Addition.java
@@ -0,0 +1,29 @@
+package Week2Assignments;
+
+public class Addition {
+	
+
+	static int SumOf2,SumOf3,num1,num2,num3;
+	
+	public static void sum(int num1,int num2)
+	{
+	
+		SumOf2=num1+num2;
+		
+		System.out.println("Sum of two numbers is " +SumOf2);
+			
+	}
+
+
+	public static void sum(int num1,int num2,int num3)
+	{
+		
+		SumOf3=num1+num2+num3;
+		System.out.println("Sum of three numbers is " +SumOf3);
+			
+	}
+
+
+
+
+}
---
+++ Week 1 - Assignment/src/Week2Assignments/Week2Assignment6.java
@@ -0,0 +1,15 @@
+package Week2Assignments;
+
+public class Week2Assignment6 extends Addition {
+	
+	
+	public static void main(String [] args)
+	
+	{
+		
+		Addition.sum(20,35);
+		Addition.sum(20,30,40);
+		
+	}
+
+}
