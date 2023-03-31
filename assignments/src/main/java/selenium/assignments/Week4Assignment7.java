962f5c64a6da9cda075573a1a587dc009ab753a0
Manju M <manjuvpm12@gmail.com>
2023-03-31 17:16:12 +0530

    week4assignment7 changes

--- assignments/src/main/java/selenium/assignments/Week4Assignment7.java
+++ assignments/src/main/java/selenium/assignments/Week4Assignment7.java
@@ -1,11 +1,19 @@
 package selenium.assignments;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Week4Assignment7 {
 	public static void main(String [] args) throws Exception
@@ -16,20 +24,32 @@ public class Week4Assignment7 {
     	options.addArguments("--start-maximized");
        
     	WebDriver driver = new ChromeDriver(options);
+    	
+    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         
         driver.get("https://hms.techcanvass.co/");  
+        
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+
         
         WebElement loginTextBox= driver.findElement(By.xpath("//input[@id='txtUserName']"));   
         loginTextBox.sendKeys("Pradnya");
         
-        WebElement passwordTextBox= driver.findElement(By.xpath("//input[@id='txtPassword']"));   
+        WebElement passwordTextBox= driver.findElement(By.xpath("//input[@id='txtPassword']")); 
         passwordTextBox.sendKeys("1994");
         
-        WebElement staySignedIn = driver.findElement(By.xpath("//input[@id='chkstaysignedin']")); 
+        WebElement staySignedIn = new WebDriverWait(driver, Duration.ofSeconds(10))
+				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='chkstaysignedin']")));
+       
+
+        //WebElement staySignedIn = driver.findElement(By.xpath("//input[@id='chkstaysignedin']")); 
         staySignedIn.click();    
         
         WebElement loginButton = driver.findElement(By.xpath("//input[@id='LoginButton']")); 
         loginButton.click();
+        
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(50))
+				.pollingEvery(Duration.ofMillis(100)).ignoring(NoSuchElementException.class).ignoring(StaleElementReferenceException.class);
         
         
         Thread.sleep(2000);
