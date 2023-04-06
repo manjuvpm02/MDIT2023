992bd58923b0e5b648ad34dbe8071a752ad2b0f1
Manju M <manjuvpm12@gmail.com>
2023-04-06 12:11:17 +0530

    Week 4 Assignements

--- assignments/src/main/java/selenium/assignments/Week4Assignment1.java
+++ assignments/src/main/java/selenium/assignments/Week4Assignment1.java
@@ -12,39 +12,39 @@ public class Week4Assignment1 {
 	
     public static void main(String[] args) throws Exception {
     	
-    	String dateInput="9-Mar-2023";
+    	String dateInput="9-Mar-2023";//Date input, this an be changed as per the need
     	
-    	String dateNo=dateInput.split("-")[0];    	
+    	String dateNo=dateInput.split("-")[0];  //splitting the dateinput to take out date number separately  	
     	String month=dateInput.split("-")[1];
     	String year=dateInput.split("-")[2];
 
 
-    	ChromeOptions options=new ChromeOptions();
-    	options.addArguments("--remote-allow-origins=*");
+    	ChromeOptions options=new ChromeOptions(); 
+    	options.addArguments("--remote-allow-origins=*");//Temporary solution to resolve web socket issue that arrived with new chrome update
        
-    	WebDriver driver = new ChromeDriver(options);
+    	WebDriver driver = new ChromeDriver(options); //Initialize driver
         
-        driver.get("https://jqueryui.com/datepicker/#buttonbar");        
+        driver.get("https://jqueryui.com/datepicker/#buttonbar"); //Navigate to URL        
         
                
-        WebElement calendarFrame = driver.findElement(By.xpath("//iframe[@class='demo-frame']"));        
-        driver.switchTo().frame(calendarFrame);
+        WebElement calendarFrame = driver.findElement(By.xpath("//iframe[@class='demo-frame']")); //Using xpath to find the calendar frame       
+        driver.switchTo().frame(calendarFrame); //switching to the calendar frame
         
-        driver.findElement(By.id("datepicker")).click();
+        driver.findElement(By.id("datepicker")).click(); //Clicking on the datepicker using the id
         
         
-        List<WebElement> dates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td"));
+        List<WebElement> dates=driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar']//td")); //Storing all the dates in the table in 'dates'
         
-        for(WebElement date : dates)
+        for(WebElement date : dates) //Going through each date in the table
         {
-        	if(date.getText().equals(dateNo))
+        	if(date.getText().equals(dateNo)) // if the date matches the input we have given then clicking on it
         	{
         		date.click();
         	}
         }
             
         
-       Thread.sleep(2000);
+       Thread.sleep(2000); //Sleep given so that the testers can see the result
         
        driver.quit();
 
--- assignments/src/main/java/selenium/assignments/Week4Assignment2.java
+++ assignments/src/main/java/selenium/assignments/Week4Assignment2.java
@@ -17,61 +17,61 @@ public class Week4Assignment2 {
 	{
 		
 
-		ChromeOptions options=new ChromeOptions();
-    	options.addArguments("--remote-allow-origins=*");
-    	options.addArguments("--start-maximized"); 
-    	WebDriver driver = new ChromeDriver(options);
-        JavascriptExecutor js = (JavascriptExecutor) driver;
+		ChromeOptions options=new ChromeOptions(); 
+    	options.addArguments("--remote-allow-origins=*");//Temporary solution to resolve web socket issue that arrived with new chrome update
+    	options.addArguments("--start-maximized"); //To have the max window size 
+    	WebDriver driver = new ChromeDriver(options); //Initialize driver
+        JavascriptExecutor js = (JavascriptExecutor) driver; //Initializing javascript executor
 
-        driver.get("https://jqueryui.com/selectmenu/"); 
-        js.executeScript("window.scrollBy(0,150)");  
+        driver.get("https://jqueryui.com/selectmenu/"); //Navigate to URL
+        js.executeScript("window.scrollBy(0,150)");  //Scrolling down to the specific location
 
         
-        Actions a = new Actions(driver);
+        Actions a = new Actions(driver); // Defining & invoking action class
         
-        WebElement demoFrame= driver.findElement(By.xpath("//iframe[@class='demo-frame']"));
-        driver.switchTo().frame(demoFrame);
+        WebElement demoFrame= driver.findElement(By.xpath("//iframe[@class='demo-frame']")); //Defining demoFrame using xpath
+        driver.switchTo().frame(demoFrame); //Switching to demoframe
         
-        String xpathForSpeedDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[1]";     
-        WebElement speedDropDown = driver.findElement(By.xpath(xpathForSpeedDropDown));
+        String xpathForSpeedDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[1]"; //Xpath for speed dropdown    
+        WebElement speedDropDown = driver.findElement(By.xpath(xpathForSpeedDropDown)); // Finding speeddropdown using xpath
         
-        speedDropDown.click();
-        a.click(speedDropDown);
-        a.sendKeys(Keys.ENTER).build().perform(); 
-        
-        
-        String xpathForFileDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[2]";     
-        WebElement fileDropDown = driver.findElement(By.xpath(xpathForFileDropDown));
-        
-        fileDropDown.click();
-        a.click(fileDropDown);
-        a.sendKeys(Keys.ARROW_DOWN).build().perform();
-        a.sendKeys(Keys.ENTER).build().perform(); 
+        speedDropDown.click(); // Clicking on dropdown arrow
+        a.click(speedDropDown); // Clicking on dropdown
+        a.sendKeys(Keys.ENTER).build().perform(); //Clicking on Enter button
         
         
-        String xpathForNumberDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[3]";     
-        WebElement numberDropDown = driver.findElement(By.xpath(xpathForNumberDropDown));
+        String xpathForFileDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[2]"; //xpath for file dropdown    
+        WebElement fileDropDown = driver.findElement(By.xpath(xpathForFileDropDown)); //Finding file dropdown using the xpath
         
-        numberDropDown.click();
-        a.click(numberDropDown);
-        a.sendKeys(Keys.ARROW_DOWN).build().perform();
-        a.sendKeys(Keys.ARROW_DOWN).build().perform();
-        a.sendKeys(Keys.ENTER).build().perform(); 
+        fileDropDown.click(); //Clicking on dropdown arrow
+        a.click(fileDropDown); //Clicking on the dropdown
+        a.sendKeys(Keys.ARROW_DOWN).build().perform(); //Pressing down arrow key
+        a.sendKeys(Keys.ENTER).build().perform();  //Pressing enter key
         
         
-        String xpathForTitleDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[4]";     
-        WebElement titleDropDown = driver.findElement(By.xpath(xpathForTitleDropDown));
+        String xpathForNumberDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[3]"; //xpath for Number dropdown 
+        WebElement numberDropDown = driver.findElement(By.xpath(xpathForNumberDropDown)); //Finding Number dropdown using the xpath
         
-        titleDropDown.click();
-        a.click(titleDropDown);
-        a.sendKeys(Keys.ARROW_DOWN).build().perform();
-        a.sendKeys(Keys.ARROW_DOWN).build().perform();
-        a.sendKeys(Keys.ENTER).build().perform(); 
+        numberDropDown.click(); //Clicking on dropdown arrow
+        a.click(numberDropDown); //Clicking on the dropdown
+        a.sendKeys(Keys.ARROW_DOWN).build().perform(); //Pressing down arrow key
+        a.sendKeys(Keys.ARROW_DOWN).build().perform(); //Pressing down arrow key
+        a.sendKeys(Keys.ENTER).build().perform(); //Pressing enter key
+        
+        
+        String xpathForTitleDropDown = "(//span[@class='ui-selectmenu-icon ui-icon ui-icon-triangle-1-s'])[4]"; //xpath for title dropdown      
+        WebElement titleDropDown = driver.findElement(By.xpath(xpathForTitleDropDown)); //Finding title dropdown using the xpath
+        
+        titleDropDown.click(); //Clicking on dropdown arrow
+        a.click(titleDropDown); //Clicking on the dropdown
+        a.sendKeys(Keys.ARROW_DOWN).build().perform(); //Pressing down arrow key
+        a.sendKeys(Keys.ARROW_DOWN).build().perform(); //Pressing down arrow key
+        a.sendKeys(Keys.ENTER).build().perform(); //Pressing enter key
         
         
        
         
-        driver.quit();
+        driver.quit(); //Quitting the driver
              
 		
 		
