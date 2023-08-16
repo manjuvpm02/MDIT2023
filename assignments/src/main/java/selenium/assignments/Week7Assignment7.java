a5247e848aa32747b4ec4a71a16f7d299fde4a3d
manjuvpm02 <manjuvpm12@gmail.com>
2023-08-16 13:23:32 +0530

    Week7Assignment7.java
    Week 7 assignment 7 opening in specific browser

---
+++ assignments/src/main/java/selenium/assignments/Week7Assignment7.java
@@ -0,0 +1,28 @@
+package grid;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+public class Week7Assignment7 {
+
+	@Test
+	public void GoogleHomePage() throws MalformedURLException {
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setCapability("browserName", "chrome");
+		chromeOptions.setCapability("se:name", "Opening Google"); 
+		chromeOptions.setCapability("se:sampleMetadata", "Sample metadata value"); 
+		chromeOptions.setCapability("browserVersion", "104");
+		WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444"), chromeOptions);
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		driver.findElement(By.name("q")).sendKeys("Moolya Software");
+		driver.close();
+	}
+}
